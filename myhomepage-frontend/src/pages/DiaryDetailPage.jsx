9df57841fece7bb5d70a846bca70085a074346a6
jiwoo1114 <isy0620@naver.com>
2025-01-15 18:14:30 +0900

    up

--- myhomepage-frontend/src/pages/DiaryDetailPage.jsx
+++ myhomepage-frontend/src/pages/DiaryDetailPage.jsx
@@ -1,10 +1,7 @@
 import DiaryDetail from '../components/post/DiaryDetail'
 
-
 function DiaryDetailPage() {
-    return ( 
-        <DiaryDetail/>
-     );
+   return <DiaryDetail />
 }
 
-export default DiaryDetailPage;
+export default DiaryDetailPage
